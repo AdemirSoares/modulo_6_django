b26ed2a4813bd12a4cddc8b20258fc4018adcad6
ademirsoares <ademir@licitaronline.com>
2025-05-25 10:42:18 -0300

    Criando o Django admin

--- modulo_6_django/blog/models.py
+++ modulo_6_django/blog/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+# Defina as opções de status aqui
+STATUS_CHOICES = [
+    ('draft', 'Rascunho'),
+    ('published', 'Publicado'),
+]
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=250)
+    slug = models.SlugField(max_length=250, unique_for_date='publish')
+    author = models.ForeignKey(User, on_delete=models.CASCADE, default=1)
+    content = models.TextField()
+    publish = models.DateTimeField(auto_now_add=True)
+    status = models.CharField(max_length=10, choices=STATUS_CHOICES, default='draft')
